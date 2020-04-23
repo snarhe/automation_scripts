2dd1dc46d40f9053f1b6838186b3210ea72c3b4c
snarhe <47171992+snarhe@users.noreply.github.com>
2020-04-23 12:40:56 +0530

    update signature
    update signature

--- Repo_Sync_RHEL7.sh
+++ Repo_Sync_RHEL7.sh
@@ -2,9 +2,9 @@
 #Purpose : Download updated Packages and create repository
 #Date : 30th Sep 2016
 #Author : Sunil Narhe
-#Modify<01> : 23rd May 2017 -- Add Download Errata screipt -- Sunil Narhe
+#Modify<01> : 23rd May 2017 -- Add Download Errata script -- Sunil Narhe
 #Modify<02> : 23rd May 2017 -- Move repodata to archive -- Sunil Narhe
-#Modify<03> : 15th Jun 2017 -- Add Carbon coy to 'DL Global Group IT Unix' -- Sunil Narhe
+#Modify<03> : 15th Jun 2017 -- Add Carbon copy to email -- Sunil Narhe
 
 dt=`date +%Y-%m-%d`
 dt1=`date`
@@ -92,17 +92,13 @@ End Date : `date`
 
 Thanks & Regards,
 _______________________________________________________________________
-ITICS Unix Team | Group IT
+Unix Team
 
-Capgemini India | Mumbai – Airoli
 
-Address: Building No.8, MindSpace, Thane-Belapur Road, Airoli,Navi Mumbai 400708, India.
-www.domainname.com
-People matter, results count.
 _______________________________________________________________________
 Connect with us on
 
-NOTE :: “Please contact NE, ITICS Unix Team for faster response
+NOTE :: “Please contact Unix Team for faster response
 
 " | mail -r spacewalk@domainname.com -s "[North]RHEL-7 Reposysnc Status `date +%d-%b-%Y`" groupitunix.global@domainname.com > /var/log/reposync_mail.log
 
